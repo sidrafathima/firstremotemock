c00375fb056235b66a69a1b7a07891c6ac46071a
sidra <sidra@gmail.com>
2024-05-22 06:16:45 +0000

    added f1and f2

---
+++ f1.sh
@@ -0,0 +1,8 @@
+#i/bin/bash
+echo "search the file"
+read $myfile
+if [-f  $myfile exitsts ]
+then
+	echo "file is present"
+else " no fille exists"
+fi
---
+++ file2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "welcome to devops screen:"
+read $sentence
+
+echo "Enter the sentence :"
+read $sentence
+
+new_sentence=$(echo "$sentence")
+
+echo "Original sentence: $sentence"
+echo "Modified sentence: $new_sentenc
