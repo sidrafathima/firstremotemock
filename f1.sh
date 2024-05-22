19f1fe6aa9a4ee0febccff9e37d38daf3b9839ad
sidra <sidra@gamil.com>
2024-05-22 06:56:19 +0000

    f1 chnged and added

--- f1.sh
+++ f1.sh
@@ -1,8 +1,5 @@
 #i/bin/bash
 echo "search the file"
-read $myfile
-if [-f  $myfile exitsts ]
-then
-	echo "file is present"
-else " no fille exists"
-fi
+read $file
+echo " enter the filename :$file"
+
